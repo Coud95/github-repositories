cb28d246aac390562845706824b9716aea48cc4a
Michal Bareja <michal.bareja@allegro.pl>
2019-07-25 12:50:41 +0200

    initial commit

---
+++ src/main/java/pl/allegro/recruitment/github/GithubRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package pl.allegro.recruitment.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoriesApplication.class, args);
+    }
+
+}

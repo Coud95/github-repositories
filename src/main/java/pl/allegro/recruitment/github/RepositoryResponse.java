482acac1ca2f59468c181e24167ea1f609f952a1
Coud95 <nikielprzemek@gmail.com>
2019-08-03 20:22:45 +0200

    added set of end-to-end tests

--- src/main/java/pl/allegro/recruitment/github/RepositoryResponse.java
+++ src/main/java/pl/allegro/recruitment/github/RepositoryResponse.java
@@ -2,12 +2,10 @@ package pl.allegro.recruitment.github;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
-@Setter
 public class RepositoryResponse {
     @JsonAlias("full_name")
     String fullName;
--- src/test/java/pl/allegro/recruitment/github/GithubRepositoriesApplicationTests.java
+++ src/test/java/pl/allegro/recruitment/github/GithubRepositoriesApplicationTests.java
@@ -2,15 +2,40 @@ package pl.allegro.recruitment.github;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GithubRepositoriesApplicationTests {
 
+    @Autowired
+    private RepositoryService repositoryService;
+
     @Test
-    public void contextLoads() {
+    public void shouldReturnStatusCode200WhenRepositoryExist() {
+        ResponseEntity response = repositoryService.getRepositoryResponse("Coud95", "HungryCat");
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
+    @Test
+    public void shouldReturnStatusCode404WhenThereIsNoSuchRepository() {
+        ResponseEntity response = repositoryService.getRepositoryResponse("Coud95", "WrongRepositoryName");
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void shouldReturnGihubRepositoryDetails() {
+        ResponseEntity response = repositoryService.getRepositoryResponse("Coud95", "HungryCat");
+        assertThat(response.getBody()).hasFieldOrPropertyWithValue("fullName", "Coud95/HungryCat")
+                .hasFieldOrPropertyWithValue("description", "Simple arcade game written with libGDX.")
+                .hasFieldOrPropertyWithValue("cloneUrl", "https://github.com/Coud95/HungryCat.git")
+                .hasFieldOrProperty("stars")
+                .hasFieldOrProperty("createdAt");
+    }
 }
